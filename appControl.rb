5d815dd94b68ec99c851c145643110d6976c1bd4
Whasame <coolmaxalt@gmail.com>
2015-07-19 15:45:52 +0000

    final appControl and questions page, vicitm and non victim coming soon

--- appControl.rb
+++ appControl.rb
@@ -19,7 +19,6 @@ class MyApp < Sinatra::Base
 	post '/questions' do
 		curr_question = params[:question]
 		@answer = params[:ans]
-		@back = params[:back]
 		
 		if @answer == "Yes"
 			@ans_value=0
@@ -28,59 +27,21 @@ class MyApp < Sinatra::Base
 		end
 
 		map={
-			"Are you under 18 years of age? (Please answer truthfuly)" => {:next_q => ["Are you being forced to do anything at all?", "Are you trading sex for anything of monetary value?"], :remove => []},
+			"Are you under 18 years of age? (Please answer truthfuly)" => {:next_q => ["Are you being forced to do anything?", "Are you trading sex acts for anything of monetary value?"], :remove => []},
 			
-			"Are you being forced to do anything at all?" => {:next_q => ["r", "Do you want to point and click click click...?"], :remove => []},
+			"Are you being forced to do anything?" => {:next_q => ["Are you performing any sex acts?", "Do you have a boyfreind or girlfreind?"], :remove => []},
 			
-			"Are you on drugs?" => {:next_q =>["rez", "Do you want to feel like you are?"], :remove => []},
+			"Are you performing any sex acts?" => {:next_q =>["Victim", "NotVictim"], :remove => []},
 			
-			"Do you want to feel like you are?" => {:next_q => ["Dangerously Flashing Lights Game", "Is your timing good?"], :remove => []},
+			"Do you have a boyfreind or girlfreind?" => {:next_q => ["Does he/she engage in sexual acts with you?", "Are you participating in any sex acts?"], :remove => []},
 			
-			"Is your timing good?" => {:next_q => ["You want to rock out?", "Are 2 buttons too many for you?"], :remove => []},
+			"Are you trading sex acts for anything of monetary value?" => {:next_q => ["Victim", "Are you being forced to perform any sex acts?"], :remove => []},
 			
-			"You want to rock out?" => {:next_q => ["rockband", "ikaruba"], :remove => []},
+			"Are you participating in any sex acts?" => {:next_q => ["Victim", "NotVictim"], :remove => []},
 			
-			"Are 2 buttons too many for you?" => {:next_q => ["pacman", "punchout"], :remove => []},
+			"Are you being forced to perform any sex acts?" => {:next_q => ["Victim", "NotVictim"]},
 			
-			"Do you want to point and click click click...?" => {:next_q => ["Do you plan on playing for the next week straight?", "Do you want the hardest game ever?"], :remove => []},
-			
-			"Do you want the hardest game ever?" => {:next_q => ["unknow", "monkeyball"], :remove => []},
-			
-			"Do you plan on playing for the next week straight?" => {:next_q => ["wow", "diablo"], :remove => []},
-			
-			"Are you trading sex for anything of monetary value?" => {:next_q => ["minecraft", "Do you want to solve some puzzles?"], :remove => []},
-			
-			"Do you want to solve some puzzles?" => {:next_q => ["Do you feel the need for a storyline as well?", "Do you enjoy simulations?"], :remove => []},
-			
-			"Do you feel the need for a storyline as well?" => {:next_q => ["Are you an Edward Gorey fan?", "Kick it oldschool?"], :remove => []},
-			
-			"Are you an Edward Gorey fan?" => {:next_q => ["limbo", "Looking for crazy Japanese awesomeness?"], :remove => []},
-			
-			"Kick it oldschool?" => {:next_q => ["tetris", "lumines"], :remove => []},
-			
-			"Looking for crazy Japanese awesomeness?" => {:next_q => ["katamari_damacy", "Yes for shiny textures and No for indi-magic?"], :remove => []},
-			
-			"Yes for shiny textures and No for indi-magic?" => {:next_q => ["portal", "braid"], :remove => []},
-		
-		"Do you enjoy simulations?" => {:next_q => ["Do you want to be in charge of a City?", "Do you have multiple local players?"], :remove => []},
-	
-	"Do you want to be in charge of a City?" => {:next_q => ["cities_skyline", "sims"], :remove => []},
-
-			"Do you have multiple local players?" => {:next_q => ["Button mashable?", "Do you want to lose yourself in another world?"], :remove => []},
-			
-			"Do you want to lose yourself in another world?" => {:next_q => ["Under Construction", "Under Construction"], :remove => []},
-			
-			"Button mashable?" => {:next_q => ["Does realistic violence upset you?", "Do you own any official team jerseys?"], :remove => []},
-			
-			"Does realistic violence upset you?" => {:next_q => ["super_smash_bros", "Do you like big-ass weapons?"], :remove => []},
-			
-			"Do you own any official team jerseys?" => {:next_q => ["easports", "Does the sound of squealing tires excite you?"], :remove => []},
-			
-			"Do you like big-ass weapons?" => {:next_q => ["soulcalibur", "Do you want to tear out your opponent's spine?"], :remove => []},
-			
-			"Does the sound of squealing tires excite you?" => {:next_q => ["Under Construction", "Under Construction"], :remove => []},
-			
-			"Do you want to tear out your opponent's spine?" => {:next_q => ["mortal_kombat", "unkown"], :remove => []},
+			"Does he/she engage in sexual acts with you?" => {:next_q => ["Victim", "NotVictim"]}
 			}
 		
 		@questions = map[curr_question][:next_q][@ans_value]
--- models/model.rb
+++ models/model.rb
@@ -1,39 +1,39 @@
-class GameList
+# class GameList
 	
-	attr_accessor :game_list
+# 	attr_accessor :game_list
 	
-	def initialize
-		@game_list=["wow", 
-		"minecraft", 
-		"diablo", 
-		"rez", 
-		"monkeyball", 
-		"tetris", 
-		"ikaruga", 
-		"lumines", 
-		"limbo", 
-		"pacman", 
-		"punchout", 
-		"braid", 
-		"portal", 
-		"supersmashbros", 
-		"sims", 
-		"citiesskyline", 
-		"simcity", 
-		"easports", 
-		"mortalkombat", 
-		"mariokart", 
-		"rockband"]
-	end
+# 	def initialize
+# 		@game_list=["wow", 
+# 		"minecraft", 
+# 		"diablo", 
+# 		"rez", 
+# 		"monkeyball", 
+# 		"tetris", 
+# 		"ikaruga", 
+# 		"lumines", 
+# 		"limbo", 
+# 		"pacman", 
+# 		"punchout", 
+# 		"braid", 
+# 		"portal", 
+# 		"supersmashbros", 
+# 		"sims", 
+# 		"citiesskyline", 
+# 		"simcity", 
+# 		"easports", 
+# 		"mortalkombat", 
+# 		"mariokart", 
+# 		"rockband"]
+# 	end
 	
-	def remove(remove_list)
-		remove_list.each do |del|
-			@game_list.delete(del)
-		end
- 		return @game_list
-	end
+# 	def remove(remove_list)
+# 		remove_list.each do |del|
+# 			@game_list.delete(del)
+# 		end
+#  		return @game_list
+# 	end
 	
-def game_list
-	@game_list
-end
-end
+# def game_list
+# 	@game_list
+# end
+# end
